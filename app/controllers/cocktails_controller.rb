41e5c1cb7c27e1617b59238e3c3a0122bf5e6a7b
Amaury Chaboche <amaury.chaboche@gmail.com>
2019-02-15 17:11:54 +0100

    done

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -24,7 +24,7 @@ class CocktailsController < ApplicationController
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name, :doses, :ingredients, :photo)
+    params.require(:cocktail).permit(:name, :doses, :ingredients, :photo, :reviews)
   end
 
   def find_cocktail
